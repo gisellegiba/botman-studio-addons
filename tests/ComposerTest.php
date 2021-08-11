f3ef7fc17bb9a5d46e31a7138064cab4f7ea6761
gisellegiba <gisellegiba@gmail.com>
2021-08-11 10:39:15 +1000

    Update ComposerTest.php

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -9,38 +9,38 @@ class ComposerTest extends TestCase
 {
     protected function tearDown(): void
     {
-//         m::close();
+        m::close();
     }
 
     /** @test */
-//     public function it_install_package()
-//     {
-//         $composer = $this->getMockedComposer();
+    public function it_install_package()
+    {
+        $composer = $this->getMockedComposer();
 
-//         $composer->install('foo/bar', function () {
-//             $this->assertTrue(true);
-//         });
-//     }
+        $composer->install('foo/bar', function () {
+            $this->assertTrue(true);
+        });
+    }
 
-//     protected function getMockedComposer()
-//     {
-//         $composer = m::mock('BotMan\Studio\Composer[getProcess]', [new Filesystem(), __DIR__])
-//             ->shouldAllowMockingProtectedMethods();
-//         $process = m::mock('Symfony\Component\Process\Process');
+    protected function getMockedComposer()
+    {
+        $composer = m::mock('BotMan\Studio\Composer[getProcess]', [new Filesystem(), __DIR__])
+            ->shouldAllowMockingProtectedMethods();
+        $process = m::mock('Symfony\Component\Process\Process');
 
-//         if (version_compare(Application::VERSION, '5.8.0', '<')) {
-//             $process->shouldReceive('setCommandLine')->with('composer require foo/bar')->andReturnSelf();
-//             $composer->shouldReceive('getProcess')->once()->andReturn($process);
-//         } else {
-//             $composer->shouldReceive('getProcess')->once()->with(['composer', 'require', 'foo/bar'])->andReturn($process);
-//         }
+        if (version_compare(Application::VERSION, '5.8.0', '<')) {
+            $process->shouldReceive('setCommandLine')->with('composer require foo/bar')->andReturnSelf();
+            $composer->shouldReceive('getProcess')->once()->andReturn($process);
+        } else {
+            $composer->shouldReceive('getProcess')->once()->with(['composer', 'require', 'foo/bar'])->andReturn($process);
+        }
 
-//         $process->shouldReceive('run')->once()->with(m::type('Closure'))->andReturnUsing(function ($callable) {
-//             $callable();
+        $process->shouldReceive('run')->once()->with(m::type('Closure'))->andReturnUsing(function ($callable) {
+            $callable();
 
-//             return 0;
-//         })->shouldReceive('isSuccessful')->once()->andReturnTrue();
+            return 0;
+        })->shouldReceive('isSuccessful')->once()->andReturnTrue();
 
-//         return $composer;
-//     }
+        return $composer;
+    }
 }
